61e16f96bca97daea92d8ba2c14d388f6575172e
rafia9005 <rafia9005@gmail.com>
2024-06-09 19:48:07 +0700

    add: config and .env

---
+++ src/config/app.ts
@@ -0,0 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const portServe = process.env.PORT || 3000;
+
+export { portServe };
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { portServe } from "./config/app";
 import router from "./router/route";
 
 const app = new Hono();
 
 app.route("/", router);
 
-const port = 3000;
+const port: any = portServe;
 console.log(`Server is running on port ${port}`);
 
 serve({
   fetch: app.fetch,
   port,
 });
-
